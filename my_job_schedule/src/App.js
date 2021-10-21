99573e8402fc7757ce573c97b29f9b1ee9ca0eb9
LEANDRO <clperez029@misena.edu.co>
2021-10-21 16:14:05 -0500

    Se avanza vista calendario en Modulo StaftSchedule

--- my_job_schedule/src/App.js
+++ my_job_schedule/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import IndexPage from './modules/IndexPage/IndexPage';
+import StaffSchedule from './components/StaffSchedule/StaffSchedule';
 
 function App() {
   return (
     <div className="App w-100">
       <header className="containerApp">
-        <IndexPage/>
+        {/* <IndexPage/> */}
+        <StaffSchedule/>
       </header>
     </div>
   );
