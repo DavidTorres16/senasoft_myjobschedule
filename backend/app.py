df37d1bb92272075f6914c22b68bcb2df9bbf7d4
unknown <Administrador@ARMDFPCCIFSP016.FormacionSena.RED>
2021-10-21 17:41:57 -0500

    Retornar Autorización

--- backend/app.py
+++ backend/app.py
@@ -85,40 +85,47 @@ def verifyToken():
 
 @app.route('/registerWorkshift',methods=["POST"])
 def registerWorkshift():
-    data=request.json()
-    id=data["id"]
-    staffid=data["staffid"]
-    shiftDay=data["shift_day"]
-    starttime=data["starttime"]
-    finishtime=data["finishtime"]
-    patientsgroup=data["patientsgroup"]
-    cur = mysql.connection.cursor()
-    alreadyExist= cur.fetchall()
-    mysql.connection.commit()
-    cur.close()
-    if alreadyExist == None:
+    try:
+
+        data=request.json()
+        id=data["id"]
+        staffid=data["staffid"]
+        shiftDay=data["shift_day"]
+        starttime=data["starttime"]
+        finishtime=data["finishtime"]
+        patientsgroup=data["patientsgroup"]
         cur = mysql.connection.cursor()
-        cur.execute("INSERT INTO workshift (`shiftid`, `staffid`, `shfitDay`, `starttime, `finishtime`, `patientsgroup` ) VALUES(%s,%s,%s,%s,%s,%s,%s,%s)",(id,staffid,staffid,shiftDay,starttime,finishtime,patientsgroup))
+        alreadyExist= cur.fetchall()
         mysql.connection.commit()
         cur.close()
-        return jsonify(exist = False)
-    else:
-        return jsonify(exist = True)
-    
+        if alreadyExist == None:
+            cur = mysql.connection.cursor()
+            cur.execute("INSERT INTO workshift (`shiftid`, `staffid`, `shfitDay`, `starttime, `finishtime`, `patientsgroup` ) VALUES(%s,%s,%s,%s,%s,%s,%s,%s)",(id,staffid,staffid,shiftDay,starttime,finishtime,patientsgroup))
+            mysql.connection.commit()
+            cur.close()
+            return jsonify(exist = False)
+        else:
+            return jsonify(exist = True)
+    except :
+        return jsonify({"Message":"Datos Faltantes"})
 #Se crea el paciente
-@app.route('/patientRegistry')
+@app.route('/patientRegistry', methods=["POST"])
 def patientRegistry():
     data=request.json
-    patientype=data["patientype"]
-    groupquantity=data["patientsNumber"]
-    serviceHours=data["serviceHours"] 
-    cur = mysql.connection.cursor()
-    cur.execute(" INSERT INTO patientsgroup` (`groupquantity`, `patientype`,servicehours )  VALUES(%s,%s,%s)",(groupquantity,patientype,serviceHours))
-    mysql.connection.commit()
-    cur.close()
-    return jsonify(data)
     #se debe crear el número de pacientes a atender 
-    
+    try:
+        patientype=data["patientype"]
+        groupquantity=data["patientsNumber"]
+        erviceHours=data["serviceHours"]
+        cur = mysql.connection.cursor()
+        cur.execute(" INSERT INTO patientsgroup (`groupquantity`, `patientype`,servicehours )  VALUES(%s,%s,%s)",(groupquantity,patientype,serviceHours))
+        mysql.connection.commit()
+        cur.close()
+        return jsonify({"Message":"Pacientes Registrados"})
+
+
+    except : 
+        return jsonify({"Message":"Faltan datos"})
 
     cur.execute(f"SELECT * FROM patientsgroup WHERE gropuquantity = '{gropuquantity}' ")
     if data.fetchall()>1:
@@ -130,24 +137,35 @@ def patientRegistry():
 
 
 def cronograma():
+
+    # token
+
+
     data=request.json
-    serviceHours=data["serviceHours"]
-    patientsNumber=data["patientsNumber"]         
-    gropuquantity=data["gropuquantity"]
-    patientype=data["patientype"]
-    turno=data["turno"]
-    cur = mysql.connection.cursor()
-
-
     #se debe crear el número de pacientes a atender 
-    # creo la cantidad de  pacientes requeridos pacientes
-    cur.execute(f"SELECT * FROM patientsgroup WHERE gropuquantity = '{gropuquantity}' ")
-    if data.fetchall()>1:
+    try:
+        patientype=data["patientype"]
+        groupquantity=data["patientsNumber"]
+        serviceHours=data["serviceHours"]
+        cur = mysql.connection.cursor()
+        cur.execute(" INSERT INTO patientsgroup (`groupquantity`, `patientype`,servicehours )  VALUES(%s,%s,%s)",(groupquantity,patientype,serviceHours))
         mysql.connection.commit()
-        cur.execute(f"SELECT * FROM staff WHERE specialities= '{patientype}'")
-    else:
-        pass
-    # buscar si hay enfermeras para ese tipo de paciente 
+        cur.close()
+        return jsonify({"Message":"Pacientes Registrados"})
+        #se debe crear el número de pacientes a atender 
+        # creo la cantidad de  pacientes requeridos pacientes
+
+    except : 
+        return jsonify({"Message":"Faltan datos"})
+
+
+
+@app.route('/indexPage',methods=["POST",'GET'])
+def retorno():
+    token=request.headers["Authorization"]
+    return jsonify({"Token retornar":token})
+
+
 
 
 if __name__ == '__main__':
