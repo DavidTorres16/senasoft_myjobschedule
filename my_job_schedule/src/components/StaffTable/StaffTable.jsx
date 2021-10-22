8bf3652135b11620cd17e756d07fc949c12712d7
LEANDRO <clperez029@misena.edu.co>
2021-10-21 23:49:20 -0500

    Se agregan estilos inciales a vista ModifyStaff

--- my_job_schedule/src/components/StaffTable/StaffTable.jsx
+++ my_job_schedule/src/components/StaffTable/StaffTable.jsx
@@ -1,12 +1,13 @@
+import './Style.css'
 import React, {useEffect,useState} from 'react'
 import { DangerActionButton } from '../ActionButton/ActionButton'
 import { NormalActionButton } from '../ActionButton/ActionButton'
 
 export default function StaffTable() {
     return (
-        <div>
-            <table>
-                <thead>
+        <div className="d-flex flex-column bg-containerApp justify-content-center align-items-center">
+            <table className="table bg-table-app tableDashboard align-middle">
+                <thead className="text-center fw-bold">
                     <th>Nombre</th>
                     <th>Especialidad</th>
                     <th>Operaciones</th>
@@ -15,10 +16,19 @@ export default function StaffTable() {
                     <tr>
                         <td>texto Ejemplo</td>
                         <td>texto Ejemplo</td>
-                        <td>
-                            <DangerActionButton/>
-                            <NormalActionButton/>
+                        <td className="column-grid">
+                                <NormalActionButton/>
+                                <DangerActionButton/>
                         </td>
+
+                        {/* <td className="row justify-content-center align-items-center w-100 marginNone">
+                            <div className="col-md-5 py-1  w-75 " >
+                                <NormalActionButton/>
+                            </div>
+                            <div className="col-md-5 py-1 w-75">
+                                <DangerActionButton/>
+                            </div>
+                        </td> */}
                     </tr>
                 </tbody>
             </table>
