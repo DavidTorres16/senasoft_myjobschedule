94324f328afc3f7d39d6c483509cc854ef29d3cf
David Torres <Administrador@ARMDFPCCIFSP014.FormacionSena.RED>
2021-10-21 22:40:49 -0500

    Se crear maquetas básicas de algunos componentes

--- my_job_schedule/src/modules/IngresarDatos/IngresarDatos.jsx
+++ my_job_schedule/src/modules/IngresarDatos/IngresarDatos.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import { PatientAsignationCardVent } from '../../components/PatientAsignationCard/PatientAsignationCard'
+import { DangerActionButton } from '../../components/ActionButton/ActionButton'
 
 export default function IngresarDatos() {
     return (
         <div className="container d-flex justify-content-center align-items-center w-100 h-100">
+            <div>
+                <DangerActionButton/>
+            </div>
             <div className="column-grid">
                 <PatientAsignationCardVent styles="1"/>
                 <PatientAsignationCardVent styles="2"/>
